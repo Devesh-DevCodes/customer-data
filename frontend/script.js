4c2be43f1fc89368d2f7cb6dbfbd47f616e88498
Devesh Verma <deveshlucky200@gmail.com>
2024-11-07 05:11:28 +0530

    check

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://customer-data-bknd.onrender.com/api/customers';
+const API_URL = 'https://customer-data-bknd.onrender.com/api/customers' || "http://localhost:3000";
 
 fetch(API_URL)
   .then(response => response.json())
